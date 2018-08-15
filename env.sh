381261fe85e240c1427f6b0780146fa7d2aeb668
Abhishek Varanasi <varanasi.abhishek@outlook.com>
2018-08-15 14:15:05 -0400

    Delete env.sh

--- env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-gcloud auth activate-service-account --key-file ./validation-193604-3a770385fc34.json
-set GOOGLE_APPLICATION_CREDENTIALS=./validation-193604-3a770385fc34.json
-set PROJECT_ID=validation-193604
-set BUCKET_ID=validation-backup
